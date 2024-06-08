6771931b939484df3813152a0f829357cd1aedcd
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2024-06-08 15:51:30 -0300

    refactor: add controller

---
+++ src/controller/wordOfTheDay.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { getWordOfDay } from "@/scrappers/getWordOfDay";
+
+export const getWordOfDayJsonController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    let data = await getWordOfDay();
+
+    const { image, ...rest } = data;
+
+    res.json(rest);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao buscar palavra do dia");
+  }
+};
+
+export const getWordOfDayImageController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    let data = await getWordOfDay();
+
+    const { image, ...rest } = data;
+
+    //@ts-ignore
+    const imgBuffer = Buffer.from(image, "base64");
+
+    res.writeHead(200, {
+      "Content-Type": "image/jpeg", // ou 'image/png' dependendo do tipo da imagem
+      "Content-Length": imgBuffer.length,
+    });
+
+    res.end(imgBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao buscar imagem");
+  }
+};
--- src/routes/dayWord.route.ts
+++ src/routes/dayWord.route.ts
@@ -1,40 +1,13 @@
 import { Router } from "express";
-import { getWordOfDay } from "@/scrappers/getWordOfDay";
+import {
+  getWordOfDayImageController,
+  getWordOfDayJsonController,
+} from "@controller/wordOfTheDay.controller";
 
 const dayRoute = Router();
 
-dayRoute.get("/json/word", async (req, res) => {
-  try {
-    let data = await getWordOfDay();
+dayRoute.get("/json/word", getWordOfDayJsonController);
 
-    const { image, ...rest } = data;
-
-    res.json(rest);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Erro ao buscar palavra do dia");
-  }
-});
-
-dayRoute.get("/image/word", async (req, res) => {
-  try {
-    let data = await getWordOfDay();
-
-    const { image, ...rest } = data;
-
-    //@ts-ignore
-    const imgBuffer = Buffer.from(image, "base64");
-
-    res.writeHead(200, {
-      "Content-Type": "image/jpeg", // ou 'image/png' dependendo do tipo da imagem
-      "Content-Length": imgBuffer.length,
-    });
-
-    res.end(imgBuffer);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Erro ao buscar imagem");
-  }
-});
+dayRoute.get("/image/word", getWordOfDayImageController);
 
 export default dayRoute;
