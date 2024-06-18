2cd4f1cb341512449f27b673e2c4648b17504c13
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2024-06-17 23:40:03 -0300

    feat: add cors for only my front-end

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -3,13 +3,38 @@ import { Request, Response, NextFunction } from "express";
 function enableCORS(req: Request, res: Response, next: NextFunction) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
 
   next();
+}
+
+export function onlyAllowCorsInMyClient(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const allowedOrigins = [process.env.FRONTEND_URL as string];
+
+  if (allowedOrigins.includes(req.headers.origin as string)) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, OPTIONS, POST, PUT, DELETE"
+    );
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+
+    next();
+  } else {
+    res.sendStatus(403);
+  }
 }
 
 export default enableCORS;
