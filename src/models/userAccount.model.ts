420749df09c3aee6744fb19720e0773e711659e4
parlandin <56051040+parlandin@users.noreply.github.com>
2025-03-04 20:13:30 -0300

    feat: add role, level, and xp fields to user account model with default values

--- src/models/userAccount.model.ts
+++ src/models/userAccount.model.ts
@@ -12,6 +12,9 @@ const UserAccountSchema: Schema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true, unique: false },
   cover: { type: String, required: false },
+  role: { type: String, required: false, default: "user" },
+  level: { type: Number, required: false, default: 1 },
+  xp: { type: Number, required: false, default: 0 },
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now },
 });
