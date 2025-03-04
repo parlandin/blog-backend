953594aa1876d4b4c20ee96d9a8ab9de3c293950
parlandin <56051040+parlandin@users.noreply.github.com>
2025-03-04 20:30:57 -0300

    feat: add JWT utility functions for signing, verifying, and decoding tokens

---
+++ src/utils/jwt.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const secret = process.env.JWT_SECRET || "secret";
+
+interface Payload {
+  username: string;
+}
+
+export const jwtSign = (payload: Payload) => {
+  return jwt.sign(payload, secret, { expiresIn: "24h" });
+};
+
+export const jwtVerify = (token: string) => {
+  return jwt.verify(token, secret) as Payload;
+};
+
+export const jwtDecode = (token: string) => {
+  return jwt.decode(token) as Payload;
+};
