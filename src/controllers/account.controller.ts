9d7bf1e989a92bbe12aac1e0df930fb84a0b03de
parlandin <56051040+parlandin@users.noreply.github.com>
2025-03-04 18:15:05 -0300

    feat: update account creation logic to include password and code, remove email handling

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -4,24 +4,14 @@ import HttpError from "../utils/httpError";
 import handleUnknownError from "../middlewares/handleUnknownError";
 
 class AccountController {
-  async getAccount(req: Request, res: Response) {
-    try {
-      const { userName } = req.params;
-      const account = await accountService.getAccount(userName);
-
-      return res.json(account);
-    } catch (error) {
-      if (error instanceof HttpError) {
-        return res.status(error.statusCode).json({ error: error.message });
-      }
-      return handleUnknownError(res, error);
-    }
-  }
-
   async createAccount(req: Request, res: Response) {
     try {
-      const { userName, email } = req.body;
-      const account = await accountService.createAccount(userName, email);
+      const { username, password, code } = req.body;
+      const account = await accountService.createAccount(
+        username,
+        code,
+        password
+      );
 
       return res.status(201).json(account);
     } catch (error) {
@@ -36,20 +26,6 @@ class AccountController {
     try {
       const { userName, code } = req.body;
       const account = await accountService.login(userName, code);
-
-      return res.json(account);
-    } catch (error) {
-      if (error instanceof HttpError) {
-        return res.status(error.statusCode).json({ error: error.message });
-      }
-      return handleUnknownError(res, error);
-    }
-  }
-
-  async validateCode(req: Request, res: Response) {
-    try {
-      const { userName, code } = req.body;
-      const account = await accountService.validateCode(userName, code);
 
       return res.json(account);
     } catch (error) {
