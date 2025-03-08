21b3213ddf3f532f9b62e142cf9bba57d0eff894
parlandin <56051040+parlandin@users.noreply.github.com>
2025-03-07 22:36:30 -0300

    feat: refactor AccountController to use decorators for routing and error handling

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -2,39 +2,31 @@ import { Request, Response } from "express";
 import accountService from "../services/account.service";
 import HttpError from "../utils/httpError";
 import handleUnknownError from "../middlewares/handleUnknownError";
+import { Controller, Post } from "../decorators/routes.decorators";
+import { CatchAll } from "../decorators/errorHandler.decorator";
 
+@Controller("/account")
+@CatchAll()
 class AccountController {
+  @Post("/create")
   async createAccount(req: Request, res: Response) {
-    try {
-      const { username, password, code } = req.body;
-      const account = await accountService.createAccount(
-        username,
-        code,
-        password
-      );
+    const { username, password, code } = req.body;
+    const account = await accountService.createAccount(
+      username,
+      code,
+      password
+    );
 
-      return res.status(201).json(account);
-    } catch (error) {
-      if (error instanceof HttpError) {
-        return res.status(error.statusCode).json({ error: error.message });
-      }
-      return handleUnknownError(res, error);
-    }
+    return res.status(201).json(account);
   }
 
+  @Post("/login")
   async login(req: Request, res: Response) {
-    try {
-      const { username, password } = req.body;
-      const account = await accountService.login(username, password);
+    const { username, password } = req.body;
+    const account = await accountService.login(username, password);
 
-      return res.json(account);
-    } catch (error) {
-      if (error instanceof HttpError) {
-        return res.status(error.statusCode).json({ error: error.message });
-      }
-      return handleUnknownError(res, error);
-    }
+    return res.json(account);
   }
 }
 
-export default new AccountController();
+export default AccountController;
