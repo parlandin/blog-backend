9295db9407be425928997c405968b8916c553e76
parlandin <56051040+parlandin@users.noreply.github.com>
2025-05-22 16:53:18 -0300

    feat: add index file to export decorators for improved modularity

---
+++ src/decorators/index.ts
@@ -0,0 +1,5 @@
+export * from "./errorHandler.decorator";
+export * from "./injectService.decorator";
+export * from "./response.decorator";
+export * from "./routes.decorators";
+export * from "./validation.decorator";
